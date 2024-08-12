f15b4131ae1c7871a8fa0ca22e389e8b5aac7b06
MaiPhuonghuynh <149395633+MaiPhuonghuynh@users.noreply.github.com>
2024-08-12 18:18:16 +0700

    banner( up để đổi ổ đĩa hết dung lượng rồi:))

---
+++ src/Page/Home/content/content.tsx
@@ -0,0 +1,15 @@
+import Content1 from "./content1/content1";
+import Content2 from "./content2/content2";
+
+
+function Content() {
+
+    return (
+        <div className={"content"}>
+            <Content1/>
+            <Content2/>
+        </div>
+    );
+}
+
+export default Content;
