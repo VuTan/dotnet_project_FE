476f177e086d5b83f5b408b158cfe741ba2f297e
PhanTTDuong <21130331@st.hcmuaf.edu.vn>
2024-08-13 21:28:12 +0700

    list product and detail p

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import RouterCustom from "./router";
 function App() {
   return (
-    <></>
+    <BrowserRouter>
+    <RouterCustom/>
+  </BrowserRouter>
   );
 }
 
