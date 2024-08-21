93ed2b4bf996c7313f866e018b4db28d73768105
MaiPhuonghuynh <149395633+MaiPhuonghuynh@users.noreply.github.com>
2024-08-21 21:42:40 +0700

    fix

--- src/Component/Header/NavBar/config/routes.ts
+++ src/Component/Header/NavBar/config/routes.ts
@@ -2,12 +2,12 @@ const routes = {
     home: '/',
     menu: '/menu',
     homeStory: '/homeStory',
-    inspiration: '/inspiration',
+    inspiration: '/cloudfee',
     store: '/store',
     login: '/login',
     signup: '/signup',
     logout: '/logout',
-
+    profile: '/profile'
 };
 
 export default routes;
